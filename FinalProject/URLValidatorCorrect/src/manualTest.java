1c4aeec7995def626d55bc6fd8454349a9c3a1f7
Mark Piccirilli <mpicc704@gmail.com>
2019-03-13 19:31:17 -0400

    manual test interface added

---
+++ FinalProject/URLValidatorCorrect/src/manualTest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class manualTest {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Enter URL: ");
+		String url = s.next();
+
+		UrlValidator url_val = new UrlValidator();
+
+		boolean a = url_val.isValid(url);
+
+		System.out.println(a);
+	}
+}
